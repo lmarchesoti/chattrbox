0cc3b3593a246da696c7d8c174cb24820ccbf0b8
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-02-12 08:40:58 -0300

    Adds dynamic resolution of mime types
    Excludes test files due to sensitive data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
+var mime = require('mime/lite');
 
 var handleError = function (err, res) {
     res.writeHead(404);
@@ -11,12 +12,15 @@ var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
 
     var filePath = extract(req.url);
+    let fileExtension = filePath.split('.').pop();
+    var mimeType = mime.getType(fileExtension);
+    console.log('Mime type is: ' + mimeType);
     fs.readFile(filePath, function (err, data) {
         if (err) {
             handleError(err, res);
             return;
         } else {
-            res.setHeader('Content-Type', 'text/html');
+            res.setHeader('Content-Type', mimeType);
             res.end(data);
         }
     });
