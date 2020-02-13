51c80f799be470302f1e5df8ae3090cc35c4ae37
Lucas Marchesoti Franco <lucas.marchesoti@callink.com.br>
2020-02-13 17:33:15 -0300

    Adds user authentication

--- websockets-server.js
+++ websockets-server.js
@@ -5,21 +5,60 @@ var ws = new WebSocketServer({
     port: port
 });
 var messages = [];
+var users = [];
+var PASSWORD = 'swordfish';
 
 console.log('websockets server started');
 
+function User(socket) {
+    this.__authenticated = false;
+    this.__socket = socket;
+
+
+    User.prototype.authenticated = function () {
+        return this.__authenticated;
+    };
+
+    User.prototype.authenticate = function () {
+        this.__authenticated = true;
+    };
+
+    User.prototype.processMessage = function (data) {
+        console.log('message received: ' + data);
+        console.log(this.authenticated());
+        if (this.authenticated()) {
+            messages.push(data);
+            users.forEach(function (user) {
+                if (user.authenticated()) {
+                    user.sendMessage(data);
+                }
+            });
+        } else {
+            if (PASSWORD === data.toLowerCase()) {
+                this.authenticate();
+
+                messages.forEach(function (msg) {
+                    this.__socket.send(msg);
+                }.bind(this));
+            }
+        }
+    };
+
+    User.prototype.sendMessage = function (data) {
+        this.__socket.send(data);
+    };
+
+    this.__socket.on('message', this.processMessage.bind(this));
+
+    this.__socket.on('close', function (data) {
+        console.log('user disconnected');
+    });
+}
+
 ws.on('connection', function (socket) {
     console.log('client connection established');
+    console.log(ws.clients.size + ' users connected.');
 
-    messages.forEach(function (msg) {
-        socket.send(msg);
-    });
-
-    socket.on('message', function (data) {
-        console.log('message received: ' + data);
-        messages.push(data);
-        ws.clients.forEach(function (clientSocket) {
-            clientSocket.send(data);
-        });
-    });
+    users.push(new User(socket));
 });
+
